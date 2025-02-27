a0eed921c24547cee23d2d08c9b5795ca66d3626
vinh <anhhotgirl8a2@gmail.com>
2025-02-28 00:06:21 +0700

    feat(setup-flow): setup flow route

---
+++ app/(root)/(client)/cakes/[cakeId]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CakeDetail = () => {
+  return (
+    <div>CakeDetail</div>
+  )
+}
+
+export default CakeDetail
--- app/(root)/(client)/layout.tsx
+++ app/(root)/(client)/layout.tsx
@@ -1,8 +1,20 @@
+"use client";
+
 import { ReactNode } from "react";
 import Footer from "@/components/shared/client/footer/footer";
 import Header from "@/components/shared/client/header/header";
+import { usePathname } from "next/navigation";
 
-const Layout = async ({ children }: { children: ReactNode }) => {
+const Layout = ({ children }: { children: ReactNode }) => {
+  const pathName = usePathname();
+
+  const isBuildLayout3D =
+    pathName.startsWith("/stores/") && pathName.endsWith("/build");
+    
+  if (isBuildLayout3D) {
+    return <>{children}</>;
+  }
+
   return (
     <div className="min-h-screen flex flex-col bg-gray-50 dark:bg-gray-950 text-gray-900 dark:text-gray-100">
       <Header />
